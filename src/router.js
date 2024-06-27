4309b680e2dc9fd1fc770a01713b7434a05fbba4
Eduardo Martelo <eduardohummer1990@gmail.com>
2024-06-27 15:27:23 -0300

    usuario router

--- src/router.js
+++ src/router.js
@@ -3,7 +3,11 @@ import Router from "vue-router";
 import Home from "./views/Home.vue";
 import Produto from "./views/Produto.vue";
 import Login from "./views/Login.vue";
-import Usuario from "./views/usuario/usuario.vue";
+import Usuario from "./views/usuario/Usuario.vue";
+import UsuarioProdutos from "./views/usuario/UsuarioProdutos.vue";
+import UsuarioEditar from "./views/usuario/UsuarioEditar.vue";
+import UsuarioCompras from "./views/usuario/UsuarioCompras.vue";
+import UsuarioVendas from "./views/usuario/UsuarioVendas.vue";
 
 Vue.use(Router);
 
@@ -28,9 +32,30 @@ export default new Router({
       component: Login,
     },
     {
-      path: "/usuario",
-      name: "usuario",
-      component: Usuario
+      path: "/Usuario",
+      component: Usuario,
+      children: [
+        {
+          path: "",
+          name: "usuario",
+          component: UsuarioProdutos
+        },
+        {
+          path: "compras",
+          name: "compras",
+          component: UsuarioCompras
+        },
+        {
+          path: "vendas",
+          name: "vendas",
+          component: UsuarioVendas
+        },
+        {
+          path: "editar",
+          name: "usuario-editar",
+          component: UsuarioEditar
+        },
+      ]
     }
   ],
   scrollBehavior() {
